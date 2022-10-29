d99052f412c10e561f2578e7557369e4e327ef99
Asadullah Kashif <s.asad18k@gmail.com>
2022-10-30 01:47:16 +0500

    implemented persisting and loading data

--- src/main.py
+++ src/main.py
@@ -1,12 +1,31 @@
 from datetime import datetime
+from classes import DirectoryNode, Memory
+import file_io
+import signal
+from menu import display_menu, user_input, exit_program
 
-from classes import FS_Node, DirectoryNode, FileNode, print_directory_structure
-from utils import display_menu, user_input
+
+root: DirectoryNode = None
+memory: Memory = None
 
 
 def main():
+    global root, memory
+
+    root, memory = file_io.load_from_file()
+
+    if not root or not memory:
+        root = DirectoryNode('root', datetime.now())
+        memory = Memory()
+
+    signal.signal(signal.SIGINT, handle_sigint)
+
     display_menu()
-    user_input()
+    user_input(root, memory)
+
+
+def handle_sigint(sig, frame):
+    exit_program(root, memory)
 
 
 if __name__ == '__main__':
